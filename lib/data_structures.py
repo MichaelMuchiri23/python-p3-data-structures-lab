b0ad6fd783f3991c140370529c8d2892bc1c63d1
Michael Muchiri <muchirimichael2222@gmail.com>
2023-12-01 00:24:11 +0300

    Done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,39 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = [obj["name"] for obj in spicy_foods]
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spicy = [obj for obj in spicy_foods if obj["heat_level"] > 5]
+    return spicy
 
 def print_spicy_foods(spicy_foods):
-    pass
+    emoji = '\U0001F336'
+    for obj in spicy_foods:
+        print(f"{obj['name']} ({obj['cuisine']}) | Heat Level: {obj['heat_level'] * emoji}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for obj in spicy_foods:
+        if obj["cuisine"] == cuisine:
+            return obj
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    emoji = '\U0001F336'
+    for obj in spicy_foods:
+        if obj["heat_level"] > 5:
+            print(f"{obj['name']} ({obj['cuisine']}) | Heat Level: {obj['heat_level'] * emoji}")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    average = 0
+    for obj in spicy_foods:
+        average += obj["heat_level"]
+
+    return(average / 3)    
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+#Case examples
+print_spicy_foods(spicy_foods)
